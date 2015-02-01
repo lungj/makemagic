8e6fc439c7bfc8a6aed3a6fb978849673faf1254
jonathan lung <lungj@sojournlabs.com>
2015-02-01 13:02:00 -0500

    OPT: Faster library detection.

--- prerequisite_functions.mk
+++ prerequisite_functions.mk
@@ -4,7 +4,8 @@ COMMAND_EXISTS = $(strip $(shell (which $(1) > /dev/null); echo $$?))
 
 # 0 if library is installed, 1 otherwise
 ifeq ($(OS), mac)
- LIBRARY_EXISTS = $(strip $(shell (brew ls $(1) > /dev/null 2> /dev/null); echo $$?))
+ INSTALLED_LIBS := $(shell brew ls)
+ LIBRARY_EXISTS = $(subst x,0,$(subst 0,1,$(subst 1,x,$(words $(filter libevent, $(INSTALLED_LIBS))))))
 else
  $(error "I don't know how to check if a library is installed on this system.")
 endif
